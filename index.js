67626cb90c5eb610bce16a0d27bdee5273af115d
Matt Currier <fingerskier@gmail.com>
2025-05-27 07:42:24 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,60 @@
+import 'dotenv/config'
+
+import http from 'http'
+import express from 'express'
+import { Server } from 'socket.io'
+import { Pool } from 'pg'
+import cors from 'cors'
+import jwt from 'jsonwebtoken'
+import os from 'os'
+
+import setupEmailer from './email.js'
+import setupOtpLogin from './otp-login.js'
+import listenRPC from './IO/RPC.js'
+import cookieParser from 'cookie-parser'
+
+const app = express()
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173']
+allowedOrigins.push(os.hostname())
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+)
+
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: { origin: allowedOrigins, credentials: true },
+})
+
+io.use((socket, next) => {
+  const token = socket.handshake.auth.token
+
+  if (!token) {
+    return next(new Error('Authentication error: Token missing'))
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return next(new Error('Authentication error: Invalid token'))
+    }
+
+    socket.user = decoded // Save decoded user info in socket instance
+    next()
+  })
+})
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.static('public'))
+
+setupEmailer(app, pool)
+setupOtpLogin(app, pool)
+listenRPC(io, pool).catch(console.error)
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('Server running on port', process.env.PORT || 3000)
+})
